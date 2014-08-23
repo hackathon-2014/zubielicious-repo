0ef49f1caf205b4989fb2579d1f9b44bf146a19d
Logan Edwards <loganwedwards@gmail.com>
2014-08-23 11:56:38 -0400

    Show message based on weather

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,25 +1,29 @@
 'use strict';
 
 angular.module('zubieliciousRepoApp')
-  .controller('MainCtrl', function ($scope, $http, Location) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope, $http, Location, $q) {
+    // $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    // });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+    // $scope.addThing = function() {
+    //   if($scope.newThing === '') {
+    //     return;
+    //   }
+    //   $http.post('/api/things', { name: $scope.newThing });
+    //   $scope.newThing = '';
+    // };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    // $scope.deleteThing = function(thing) {
+    //   $http.delete('/api/things/' + thing._id);
+    // };
+    $scope.scaler = 1;
+    $scope.weatherLoaded = false;
+
     $scope.weatherData = {};
+    var deferredWeather = $q.defer();
     Location.getLocation().then(function (data) {
       $scope.location = data;
     }).then(function () {
@@ -28,11 +32,28 @@ angular.module('zubieliciousRepoApp')
       console.log('weatherLocation ' + weatherlocation);
       $http.get(weatherlocation).success(function (weatherData) {
         $scope.weatherData.maxTemp = weatherData.main.temp_max;
-        $scope.weatherData.minTemp = weatherData.main.temp_min ;
+        $scope.weatherData.minTemp = weatherData.main.temp_min;
         $scope.weatherData.temp = weatherData.main.temp;
         $scope.weatherData.humidity = (weatherData.main.humidity / 100.0);
         // grab the first weather condition
         $scope.weatherData.currentConditions = weatherData.weather[0];
+        deferredWeather.resolve();
       });
+    });
+
+    $scope.tooHot = function() {
+      return $scope.weatherData.maxTemp || $scope.weatherData.temp > 90;
+    }
+
+    $scope.tooCold = function() {
+      return $scope.weatherData.minTemp || $scope.weatherData.temp < 60;
+    }
+
+    // main function to setup weather dependencies
+    deferredWeather.promise.then(function() {
+      if ($scope.tooHot() || $scope.tooCold()) {
+        $scope.scaler = 0.7;
+      }
+      $scope.weatherLoaded = true;
     });
   });
