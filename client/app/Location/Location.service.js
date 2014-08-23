863a1c9f039696f71348daa9392c38ee53e2fba7
Logan Edwards <loganwedwards@gmail.com>
2014-08-23 11:35:02 -0400

    Add weather and location services

---
+++ client/app/Location/Location.service.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('zubieliciousRepoApp')
+  .factory('Location', function ($q) {
+    // Service logic
+    // ...
+
+    var location = {};
+
+
+
+    function showPosition (position) {
+
+    };
+
+    function error (err) {
+      return 'error ' + err;
+    }
+
+    location.getLocation = function () {
+      var deferred = $q.defer();
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          location.lat = position.coords.latitude;
+          console.log('lat' + location.lat);
+          location.lng = position.coords.longitude;
+
+          var loc  = {lat: position.coords.latitude, lng: position.coords.longitude};
+
+          deferred.resolve(loc);
+        }, error);
+      }
+      else {
+        return 'location not supported';
+      }
+      return deferred.promise;
+    };
+
+    return location;
+
+    // Public API here
+    return {
+      getLocation: getLocation,
+    };
+  });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('zubieliciousRepoApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, Location) {
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
@@ -19,4 +19,20 @@ angular.module('zubieliciousRepoApp')
     $scope.deleteThing = function(thing) {
       $http.delete('/api/things/' + thing._id);
     };
+    $scope.weatherData = {};
+    Location.getLocation().then(function (data) {
+      $scope.location = data;
+    }).then(function () {
+      var weatherlocation = 'http://api.openweathermap.org/data/2.5/weather?lat='
+      + $scope.location.lat + '&lon=' + $scope.location.lng + '&units=imperial';
+      console.log('weatherLocation ' + weatherlocation);
+      $http.get(weatherlocation).success(function (weatherData) {
+        $scope.weatherData.maxTemp = weatherData.main.temp_max;
+        $scope.weatherData.minTemp = weatherData.main.temp_min ;
+        $scope.weatherData.temp = weatherData.main.temp;
+        $scope.weatherData.humidity = (weatherData.main.humidity / 100.0);
+        // grab the first weather condition
+        $scope.weatherData.currentConditions = weatherData.weather[0];
+      });
+    });
   });
